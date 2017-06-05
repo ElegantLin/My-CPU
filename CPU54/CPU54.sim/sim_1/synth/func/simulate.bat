3c809c242fb134ab659291d738b7b1cbc5c0c2aa
ElegantLin <elegant.lin21@gmail.com>
2017-06-06 01:37:14 +0800

    Delete some rubbish

--- CPU54/CPU54.sim/sim_1/synth/func/simulate.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-set xv_path=D:\\Xilinx\\Vivado\\2016.4\\bin
-call %xv_path%/xsim openmips_min_sopc_tb_func_synth -key {Post-Synthesis:sim_1:Functional:openmips_min_sopc_tb} -tclbatch openmips_min_sopc_tb.tcl -log simulate.log
-if "%errorlevel%"=="0" goto SUCCESS
-if "%errorlevel%"=="1" goto END
-:END
-exit 1
-:SUCCESS
-exit 0
