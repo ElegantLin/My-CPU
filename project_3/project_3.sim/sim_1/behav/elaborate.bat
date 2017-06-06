d527067e1b6ee9ac3201d6545d44ea66084818ae
ElegantLin <elegant.lin21@gmail.com>
2017-06-06 10:28:22 +0800

    Addi passed

--- project_3/project_3.sim/sim_1/behav/elaborate.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-set xv_path=D:\\Xilinx\\Vivado\\2016.4\\bin
-call %xv_path%/xelab  -wto 367d6fba6eb94f16a0e1d245323fc90d -m64 --debug typical --relax --mt 2 -L xil_defaultlib -L unisims_ver -L unimacro_ver -L secureip --snapshot openmips_min_sopc_tb_behav xil_defaultlib.openmips_min_sopc_tb xil_defaultlib.glbl -log elaborate.log
-if "%errorlevel%"=="0" goto SUCCESS
-if "%errorlevel%"=="1" goto END
-:END
-exit 1
-:SUCCESS
-exit 0
